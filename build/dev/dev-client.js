47959a7d322e7bed100730617c722515ab7542cc
liangxiaojun <fatalxiao@163.com>
2018-05-04 09:28:07 +0800

    remove build

--- build/dev/dev-client.js
+++
@@ -1,5 +0,0 @@
-require('eventsource-polyfill');
-
-const hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true');
-
-hotClient.subscribe(event => event.action === 'reload' && window.location.reload());
