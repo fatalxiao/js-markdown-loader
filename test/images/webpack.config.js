e51c3ad900097db3abd3e4bf7489c723e06db3b9
liangxiaojun <fatalxiao@163.com>
2020-04-14 14:27:36 +0800

    update images test

--- test/images/webpack.config.js
+++ test/images/webpack.config.js
@@ -1,15 +1,12 @@
 const path = require('path');
 
-console.log('__dirname::', __dirname);
-
 module.exports = {
     entry: {
         app: './test/images/app.js'
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].js',
-        publicPath: '/'
+        filename: '[name].js'
     },
     module: {
         rules: [{
