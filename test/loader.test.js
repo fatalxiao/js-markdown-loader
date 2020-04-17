58bb1d8da5f56833423855aad6e3463c6b63f6a7
liangxiaojun <fatalxiao@163.com>
2020-04-17 15:45:08 +0800

    update loader.test.js

--- test/loader.test.js
+++ test/loader.test.js
@@ -9,8 +9,6 @@ describe('loader', () => {
         const compiler = getCompiler('images.js');
         const stats = await compile(compiler);
 
-        console.log('execute(readAsset(\'main.bundle.js\', compiler, stats))::', execute(readAsset('main.bundle.js', compiler, stats)));
-
         expect(getModuleSource('./images.js', stats)).toMatchSnapshot('module');
         expect(execute(readAsset('main.bundle.js', compiler, stats))).toMatchSnapshot('result');
         expect(getWarnings(stats)).toMatchSnapshot('warnings');
